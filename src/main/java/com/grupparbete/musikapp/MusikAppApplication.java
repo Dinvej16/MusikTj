627d77fdcabe41a87aa3acb2ae03d867d4cc02dd
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-09-28 13:55:13 +0200

    Added project

---
+++ src/main/java/com/grupparbete/musikapp/MusikAppApplication.java
@@ -0,0 +1,13 @@
+package com.grupparbete.musikapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusikAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusikAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupparbete/musikapp/MusikAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupparbete.musikapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusikAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
