13ab6d9cc7bde42feaec193b148f97be907cb7e1
Isabella <90781622+S3she@users.noreply.github.com>
2022-10-01 20:30:05 +0200

    Find User by ID and username

--- src/main/java/com/grupparbete/musikapp/DAO/UserDAO.java
+++ src/main/java/com/grupparbete/musikapp/DAO/UserDAO.java
@@ -3,12 +3,25 @@ package com.grupparbete.musikapp.DAO;
 import com.grupparbete.musikapp.model.User;
 import com.grupparbete.musikapp.repository.UserRepository;
 
+import java.util.Optional;
+
 public class UserDAO {
 
     UserRepository userRepository;
 
     public void saveUser(User user) {
         userRepository.save(user);
+
+    }
+
+    public Optional <User> findByUsername (String username) {
+        return userRepository.findUserByUsername(username);
+
+    }
+
+    public User findCurrentUserById(Long id){
+        return userRepository.findUserById(id);
+
 
     }
 
--- src/main/java/com/grupparbete/musikapp/repository/UserRepository.java
+++ src/main/java/com/grupparbete/musikapp/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.grupparbete.musikapp.repository;
 
-public interface UserRepository {
+import com.grupparbete.musikapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserById(long id);
+
+    Boolean existsByUsername(String username);
+
+    Optional<User> findUserByUsername(String username);
 }
