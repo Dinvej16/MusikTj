c5472d127adb3df0ff14c12ae8ec1710248678af
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-10-01 12:24:09 +0200

    New Stuff

--- src/main/java/com/grupparbete/musikapp/service/ExternalApiCall.java
+++ src/main/java/com/grupparbete/musikapp/service/ExternalApiCall.java
@@ -1,4 +1,19 @@
 package com.grupparbete.musikapp.service;
 
+import net.minidev.json.JSONObject;
+import org.springframework.web.client.RestTemplate;
+
 public class ExternalApiCall {
+
+    private static final String apiUrl = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=dior&api_key=cb2bdec273e087b5aef93970d41e9676&format=json";
+
+    public static JSONObject fetchUserData(){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject(apiUrl, JSONObject.class);
+        try {
+            return restTemplate.getForObject(apiUrl, JSONObject.class);
+        } catch (Exception abc){
+            return null;
+        }
+    }
 }
