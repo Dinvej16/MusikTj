ba92618235aa576f1551be0d3dc05802ea215241
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-10-01 12:37:46 +0200

    Update comment

--- src/main/java/com/grupparbete/musikapp/service/ExternalApiCall.java
+++ src/main/java/com/grupparbete/musikapp/service/ExternalApiCall.java
@@ -5,8 +5,8 @@ import org.springframework.web.client.RestTemplate;
 
 public class ExternalApiCall {
 
-    //Detta är "sök" urlen som finns i LastFm API. Det som händer här är att den hämtar alla låtar som har ordet "Dior" i titlen. 
-    private static final String apiUrl = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=dior&api_key=cb2bdec273e087b5aef93970d41e9676&format=json";
+    //Detta är "sök" urlen som finns i LastFm API. Det som händer här är att den kan hitta låt utifrån både artist och låtens namn.
+    private static final String apiUrl = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=asme &api_key=cb2bdec273e087b5aef93970d41e9676&format=json";
 
     public static JSONObject fetchUserData(){
         RestTemplate restTemplate = new RestTemplate();
