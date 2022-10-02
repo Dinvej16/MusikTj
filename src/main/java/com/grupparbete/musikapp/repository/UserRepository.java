43ec8bd04f1dcaaf2e0ac3f5fec14259c54d5591
Isabella <90781622+S3she@users.noreply.github.com>
2022-10-02 20:13:53 +0200

    Find User by ID and username

--- src/main/java/com/grupparbete/musikapp/repository/UserRepository.java
+++ src/main/java/com/grupparbete/musikapp/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.grupparbete.musikapp.repository;
 
-public interface UserRepository {
+import com.grupparbete.musikapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserById(long id);
+
+
 }
