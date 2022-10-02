5b3889cc8beb1a20b178b25fa02621ceae4c8835
Isabella <90781622+S3she@users.noreply.github.com>
2022-10-02 20:22:58 +0200

    Find by ID function

--- src/main/java/com/grupparbete/musikapp/dao/UserDAO.java
+++ src/main/java/com/grupparbete/musikapp/dao/UserDAO.java
@@ -1,4 +1,19 @@
 package com.grupparbete.musikapp.dao;
 
+import com.grupparbete.musikapp.model.User;
+import com.grupparbete.musikapp.repository.UserRepository;
+
+import java.util.Optional;
+
 public class UserDAO {
+
+    UserRepository userRepository;
+
+
+    public User findCurrentUserById(Long id){
+        return userRepository.findUserById(id);
+
+    }
+
+
 }
