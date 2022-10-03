eb967d3b8bbd4546b2a2e5a2a3545669b89ded1f
Liban <libanmoh9898@gmail.com>
2022-10-03 14:56:12 +0200

    a
    a

--- src/main/java/com/grupparbete/musikapp/model/Song.java
+++ src/main/java/com/grupparbete/musikapp/model/Song.java
@@ -1,4 +1,44 @@
 package com.grupparbete.musikapp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Song {
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      Long id;
+      String name;
+      String artist;
+      String streams; // Sångens speltid
+
+      public Song(String name, String artist, String streams) {
+            this.id = id;
+            this.name = name;
+            this.artist = artist;
+            this.streams = streams;
+      }
+
+      public Song() {
+
+      }
+
+
+      public Long getId() {
+            return id;
+      }
+
+      public String getName() {
+            return name;
+      }
+
+      public String getArtist() {
+            return artist;
+      }
+
+      public String getStreams() {
+            return streams;
+      }
 }
