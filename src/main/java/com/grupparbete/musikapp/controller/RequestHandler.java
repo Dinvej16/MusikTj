70cde6ccbd4eb24293567dadf8d3a708ec60caa8
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-10-03 13:59:21 +0200

    Added stuff

--- src/main/java/com/grupparbete/musikapp/controller/RequestHandler.java
+++ src/main/java/com/grupparbete/musikapp/controller/RequestHandler.java
@@ -3,12 +3,24 @@ package com.grupparbete.musikapp.controller;
 import com.grupparbete.musikapp.service.ExternalApiCall;
 import net.minidev.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class RequestHandler {
-    @GetMapping()
-    public static JSONObject externalControllerHandler(){
+
+    @GetMapping("/Search")
+    public  JSONObject externalControllerHandler(){
         return ExternalApiCall.searchSong();
     }
+
+    @GetMapping("/TrackInfo")
+    public JSONObject externalControllerHandler2(){
+        return ExternalApiCall.getTrackInfo();
+
+
+    }
+
+
 }
--- src/main/java/com/grupparbete/musikapp/service/ExternalApiCall.java
+++ src/main/java/com/grupparbete/musikapp/service/ExternalApiCall.java
@@ -5,14 +5,26 @@ import org.springframework.web.client.RestTemplate;
 
 public class ExternalApiCall {
 
-    //Detta är "sök" urlen som finns i LastFm API. Det som händer här är att den kan hitta låt utifrån både artist och låtens namn.
-    private static final String apiUrlSearch = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=asme&api_key=cb2bdec273e087b5aef93970d41e9676&format=json";
+    //Detta är "sök" urlen som finns i LastFm API. Det som händer här är att den kan hitta låtar utifrån både artist och låtens namn.
+    private static final String apiUrlSearch = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=23&api_key=cb2bdec273e087b5aef93970d41e9676&format=json";
+
+    // Detta är "getTrackInfo" urlen som finns i LastFm API. Det som händer här är att den hämtar all info kring låten man sökt på.
+    private static final String apiUrlGetInfo = "https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=cb2bdec273e087b5aef93970d41e9676&artist=cher&track=believe&format=json";
 
     public static JSONObject searchSong(){
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getForObject(apiUrlSearch, JSONObject.class);
         try {
             return restTemplate.getForObject(apiUrlSearch, JSONObject.class);
+        } catch (Exception abc){
+            return null;
+        }
+    }
+    public static JSONObject getTrackInfo(){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject(apiUrlGetInfo, JSONObject.class);
+        try {
+            return restTemplate.getForObject(apiUrlGetInfo, JSONObject.class);
         } catch (Exception abc){
             return null;
         }
