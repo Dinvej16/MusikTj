a35af5f7f28cfb1072520359ae818aff8aaea032
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-10-03 14:35:59 +0200

    Added stuff

--- src/main/java/com/grupparbete/musikapp/controller/RequestHandler.java
+++ src/main/java/com/grupparbete/musikapp/controller/RequestHandler.java
@@ -3,6 +3,7 @@ package com.grupparbete.musikapp.controller;
 import com.grupparbete.musikapp.service.ExternalApiCall;
 import net.minidev.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class RequestHandler {
 
-    @GetMapping("/Search")
-    public  JSONObject externalControllerHandler(){
-        return ExternalApiCall.searchSong();
+    @GetMapping("/Search/{name}")
+    public  JSONObject externalControllerHandler(@PathVariable String name){
+        return ExternalApiCall.searchSong(name);
     }
 
     @GetMapping("/TrackInfo")
--- src/main/java/com/grupparbete/musikapp/service/ExternalApiCall.java
+++ src/main/java/com/grupparbete/musikapp/service/ExternalApiCall.java
@@ -6,12 +6,12 @@ import org.springframework.web.client.RestTemplate;
 public class ExternalApiCall {
 
     //Detta är "sök" urlen som finns i LastFm API. Det som händer här är att den kan hitta låtar utifrån både artist och låtens namn.
-    private static final String apiUrlSearch = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=23&api_key=cb2bdec273e087b5aef93970d41e9676&format=json";
 
     // Detta är "getTrackInfo" urlen som finns i LastFm API. Det som händer här är att den hämtar all info kring låten man sökt på.
     private static final String apiUrlGetInfo = "https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=cb2bdec273e087b5aef93970d41e9676&artist=cher&track=believe&format=json";
 
-    public static JSONObject searchSong(){
+    public static JSONObject searchSong(String name){
+        String apiUrlSearch = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + name + "&api_key=cb2bdec273e087b5aef93970d41e9676&format=json";
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getForObject(apiUrlSearch, JSONObject.class);
         try {
