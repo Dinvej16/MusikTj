701d160130dab7f8aad1444bb4ced29551f28f13
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-10-04 13:47:54 +0200

    Update UserDAO.java

--- src/main/java/com/grupparbete/musikapp/DAO/UserDAO.java
+++ src/main/java/com/grupparbete/musikapp/DAO/UserDAO.java
@@ -1,29 +1,4 @@
-package com.grupparbete.musikapp.DAO;
-
-import com.grupparbete.musikapp.model.User;
-import com.grupparbete.musikapp.repository.UserRepository;
-
-import java.util.Optional;
+package com.grupparbete.musikapp.dao;
 
 public class UserDAO {
-
-    UserRepository userRepository;
-
-    public void saveUser(User user) {
-        userRepository.save(user);
-
-    }
-
-    public Optional <User> findByUsername (String username) {
-        return userRepository.findUserByUsername(username);
-
-    }
-
-    public User findCurrentUserById(Long id){
-        return userRepository.findUserById(id);
-
-
-    }
-
-
 }
