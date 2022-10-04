1029fdca526c810418e89089cf79bf8a9c276432
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-10-04 14:33:08 +0200

    Changed repository

--- src/main/java/com/grupparbete/musikapp/repository/PlaylistRepository.java
+++ src/main/java/com/grupparbete/musikapp/repository/PlaylistRepository.java
@@ -2,7 +2,8 @@ package com.grupparbete.musikapp.repository;
 
 import com.grupparbete.musikapp.model.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface PlaylistRepository extends JpaRepository <Playlist, Integer> {
+public interface PlaylistRepository extends CrudRepository<Playlist, Integer> {
 }
 
