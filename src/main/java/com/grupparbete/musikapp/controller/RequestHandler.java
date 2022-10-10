d7e339946b03047f8b53a64723b84651db3de38e
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-10-10 16:43:39 +0200

    Update RequestHandler.java

--- src/main/java/com/grupparbete/musikapp/controller/RequestHandler.java
+++ src/main/java/com/grupparbete/musikapp/controller/RequestHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin 
+@CrossOrigin
 public class RequestHandler {
 
     @GetMapping("/Search/{name}")
