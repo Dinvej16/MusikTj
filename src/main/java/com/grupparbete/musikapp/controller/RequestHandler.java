7d04990cb2c3f01f3214d7a854afcf93af4050ed
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-10-10 16:41:06 +0200

    Update RequestHandler.java

--- src/main/java/com/grupparbete/musikapp/controller/RequestHandler.java
+++ src/main/java/com/grupparbete/musikapp/controller/RequestHandler.java
@@ -2,13 +2,11 @@ package com.grupparbete.musikapp.controller;
 
 import com.grupparbete.musikapp.service.ExternalApiCall;
 import net.minidev.json.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin 
 public class RequestHandler {
 
     @GetMapping("/Search/{name}")
